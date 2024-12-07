fc993e799819e9b7bd0e5233b7591a057085ce0f
John Marriott <john.marriott@campspot.com>
2024-12-07 07:46:23 -0600

    year 2024 day 7

---
+++ year2024/day07/1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import fileinput
+import itertools
+
+def line_calibration_result(line: str) -> int:
+    """Value of the line if it's feasible, zero otherwise"""
+    desired_value = int(line.split(":")[0])
+    operands = list(map(int, line.split(":")[1].strip().split(" ")))
+    
+    operator_permutations = list(itertools.product(["+", "*"], repeat=len(operands) - 1))
+
+    for operator_permutation in operator_permutations:
+        computed_value = operands[0]
+        for i, operator in enumerate(operator_permutation):
+            if operator == "+":
+                computed_value += operands[i + 1]
+            else:
+                computed_value *= operands[i + 1]
+
+        if computed_value == desired_value:
+            return desired_value
+        
+    return 0
+
+lines = [line.strip() for line in fileinput.input()]
+
+print(sum([line_calibration_result(line) for line in lines]))
